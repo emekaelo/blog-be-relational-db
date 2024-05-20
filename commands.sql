4df4807731431867a2bafad90e2d981d3102bc54
emekaelo <devemekaelo@gmail.com>
2024-05-20 08:21:32 +0100

    Exercise 13.1 - Exercise 13.3

---
+++ commands.sql
@@ -0,0 +1,3 @@
+insert into blogs (author, url, title) values ('John Doe', 'www.jdbooks.com', 'Greatness');
+
+insert into blogs (author, url, title, likes) values ('Harry Cole', 'www.cole.com', 'Best of both', 4);
