16c0aae8b0a071ab3089ee32df96d7f1dd1ed15a
emekaelo <devemekaelo@gmail.com>
2024-05-22 01:18:44 +0100

    Exercise 13.9 completed

--- util/middleware.js
+++ util/middleware.js
@@ -8,8 +8,8 @@ const blogFinder = async (req, res, next) => {
 const errorHandler = (error, request, response, next) => {
     if (error.name === 'CastError') {
         return response.status(400).send({error: 'malformatted id'})
-        // } else if (error.name === 'SequelizeValidationError') { // handle individual error types
-        //     return response.status(400).json({ error: error.errors })
+    } else if (error.name === 'SequelizeValidationError') { // handle individual error types
+        return response.status(400).json({error: error.errors.map(error => error.message)})
         // }  else if (error.name === 'SequelizeDatabaseError') { // handle individual error types
         //     return response.status(400).json({ error: error })
     } else if (error) { // Return errors for all error types
